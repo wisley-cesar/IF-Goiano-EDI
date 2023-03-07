bcb5716694dd516ce10e8a9445c0eeb6cd87b82e
Wisley César <116464094+wisley-cesar@users.noreply.github.com>
2023-03-06 21:35:58 -0300

    Create Questao26.java

---
+++ ATIVIDADE01/src/Questao26.java
@@ -0,0 +1,19 @@
+public class Questao26 {
+    public static void main(String[] args) {
+      int x,y;
+      x= 2;
+      y= 5;
+        System.out.println("x = "+x);
+        x++;
+        System.out.println("y = "+y);
+        y--;
+        System.out.println("/n/n novo valor de x = "+x);
+        System.out.println("/n/n novo valor de x = "+y);
+        System.out.println("");
+
+
+
+
+
+    }
+}
