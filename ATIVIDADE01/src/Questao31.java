fe68216885e32319a1a586855aa868eb3cd930bb
Wisley César <116464094+wisley-cesar@users.noreply.github.com>
2023-03-06 21:36:00 -0300

    Create Questao31.java

---
+++ ATIVIDADE01/src/Questao31.java
@@ -0,0 +1,9 @@
+public class Questao31 {
+    public static void main(String[] args) {
+        double ma;
+        ma = (8 + 9 + 7 )/3;
+        System.out.println("A media aritimetica e "+ ma );
+        System.out.println("");
+
+    }
+}
