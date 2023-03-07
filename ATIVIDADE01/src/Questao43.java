c4f33de6432d0f2fa0d0e3349c02cadbd235b1de
Wisley César <116464094+wisley-cesar@users.noreply.github.com>
2023-03-06 21:36:07 -0300

    Create Questao43.java

---
+++ ATIVIDADE01/src/Questao43.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Questao43 {
+    public static void main(String[] args) {
+        Double num, logaritmo;
+        Scanner logaritimo = new Scanner(System.in);
+        System.out.println("\nentre com o logaritmando:");
+        num = logaritimo.nextDouble();
+        logaritmo = Math.log(num) / Math.log(10);
+        System.out.println("\nlofaritimo : "+ logaritmo);
+        System.out.println("\n");
+
+
+
+    }
+}
