6fb9cbf9e486720c7847c4fd8b8be62888c8a6b8
Wisley César <116464094+wisley-cesar@users.noreply.github.com>
2023-03-06 21:35:40 -0300

    Create Questao12.java

---
+++ ATIVIDADE01/src/Questao12.java
@@ -0,0 +1,7 @@
+public class Questao12 {
+    public static void main(String[] args) {
+        System.out.println("Aprendendo algortimo!!!");
+        System.out.println("Com Anita e Guto");
+
+    }
+}
